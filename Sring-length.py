1c4fb81af6cac88f35fb3ebc7805e190515275ec
Klein254 <rayobiero62@gmail.com>
2023-02-08 12:56:22 +0300

    strings,data structures

---
+++ Sring-length.py
@@ -0,0 +1,8 @@
+#car = "Mercedes"
+#print(len(car))
+
+number_1 = int(input("Enter Number One"))
+number_2 = int(input("Enter Number Two"))
+
+result = (number_1+number_2)
+print("The Sum of the two numbers is",result)
--- String.py
+++ String.py
@@ -1,5 +1,12 @@
-city = "Nairobi"
-print(city)
+#city = "Nairobi"
+#print(city)
 
-MyCity = input("Enter your  city")
-print(MyCity)
+#MyCity = input("Enter your  city")
+#print(MyCity)
+
+age = input("Enter your age")
+user_age = int(age)
+#print(age)
+month = 12
+months = (user_age*month)
+print("Your age in months is equivalent to", months, "months")
