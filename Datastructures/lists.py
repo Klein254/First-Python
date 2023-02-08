772c0e97198b4f907f867e41e806e404afcb069d
Klein254 <rayobiero62@gmail.com>
2023-02-08 13:08:33 +0300

    strings,data structures

---
+++ Datastructures/lists.py
@@ -0,0 +1,7 @@
+#Lists are characterised using square brackets.
+#A list has an order
+#You can add a new data ina a list
+mylist = ["Sharon", "Chris", "Lora", "Mbai"]
+mylist.append("Salome")
+mylist.remove("Chris")
+print(mylist)
