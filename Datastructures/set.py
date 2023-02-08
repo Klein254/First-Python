1724a32c93a4e3ee0cca782e66d48d68d9bde860
Klein254 <rayobiero62@gmail.com>
2023-02-08 13:23:40 +0300

    strings,data structures

---
+++ Datastructures/set.py
@@ -0,0 +1,4 @@
+#for set we use curly brackets
+
+myset = {"vincent", "ray", "sam", "risley"}
+print(myset)
---
+++ Datastructures/turple.py
@@ -0,0 +1,8 @@
+#turple is defined by anormal bracket
+#turle will als keep the order
+#turple you cant modify or ad data
+#You can find the position of a particular item
+
+myturple = ("Ezra", "Peter", "Wendy", "Vanessa")
+
+print(myturple[3])
