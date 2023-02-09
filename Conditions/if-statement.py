52e2995c0047138478a634d0f9bac720388a74e8
Klein254 <rayobiero62@gmail.com>
2023-02-09 12:26:04 +0300

    operators

---
+++ Conditions/if-statement.py
@@ -0,0 +1,32 @@
+number = int(input("Enter USSD Code"))
+
+if number == 544:
+    print("Welcome to safaricom Services")
+    print("Select one of the choices below")
+    print("1;Data deals")
+    print("2;Daily deals")
+    print("3;Blaze offer")
+    print("4;Data Bundles 'No expiry")
+    print("5;Okoa Data")
+    print("6;Storo data")
+    print("7;Data Balance")
+else:
+    print("wrong USSD code")
+
+number_2 = int(input("Select an option above"))
+if number_2 == 1:
+    print("Data deals")
+elif number_2 == 2:
+    print("Daily deals")
+elif number_2 == 3:
+    print("Blaze offer")
+elif number_2 == 4:
+    print("Data Bundles 'No expiry'")
+elif number_2 == 5:
+    print("Okoa Data")
+elif number_2 == 6:
+    print("Storo data")
+elif number_2 == 7:
+    print("Data Balance")
+else:
+    print("Invalid choice")
