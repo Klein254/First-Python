7732b3527e71e7bad8c009aa6e6af50da01665a2
Klein254 <rayobiero62@gmail.com>
2023-02-09 11:39:31 +0300

    dictionaries

---
+++ Datastructures/dictionaries.py
@@ -0,0 +1,3 @@
+fruits = {"Banana": 10, "Mango": 20, "Avocado": 15, "Kiwi": 30, "Peach": 40, "Pear": 40}
+fruits["Pomegranate"] = 50
+print(fruits)
--- Datastructures/set.py
+++ Datastructures/set.py
@@ -1,4 +1,9 @@
 #for set we use curly brackets
+#order is not guaranteed
+#set you can't have duplicates
 
-myset = {"vincent", "ray", "sam", "risley"}
+myset = {"Vincent", "Ray", "Sam", "Risley"}
+myset.add("Kim")
+myset.add("Ray")
+
 print(myset)
