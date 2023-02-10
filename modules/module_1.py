9529045cd1f7baa008abac890cdc71f28f8f44c9
Klein254 <rayobiero62@gmail.com>
2023-02-10 17:35:11 +0300

    modules

---
+++ modules/module_1.py
@@ -0,0 +1,6 @@
+def add_two_numbers(num1, num2):
+    return num2 + num1
+
+
+def multiply_two_numbers(num1, num2):
+    return num1 * num2
---
+++ modules/module_2.py
@@ -0,0 +1,4 @@
+from module_1 import *
+
+print(add_two_numbers(50, 10))
+print(multiply_two_numbers(40, 10))
