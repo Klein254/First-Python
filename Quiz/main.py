afb51e79d7e4f57e6582b97ece34dbaed10e59b0
Klein254 <rayobiero62@gmail.com>
2023-02-14 12:52:55 +0300

    addition to fizzbuz

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,7 +1,12 @@
-for number in range(100):
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+for number in range(num1,num2):
     if number % 3 == 0 and number % 5 == 0:
         print(number, "Fizzbuzz")
     elif number % 3 == 0:
         print(number, "Fizz")
     elif number % 5 == 0:
         print(number, "Buzz")
+    else:
+        print(number)
